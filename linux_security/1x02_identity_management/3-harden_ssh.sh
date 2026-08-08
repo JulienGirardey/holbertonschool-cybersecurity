59281bc1d0a1e8d9a6fa21a4ae6cd72869b8993a
JulienGirardey <juliengirardey2@gmail.com>
2026-08-08 01:45:55 -0700

    refactor: remove unused functions and streamline SSH configuration script

--- linux_security/1x02_identity_management/3-harden_ssh.sh
+++ linux_security/1x02_identity_management/3-harden_ssh.sh
@@ -18,34 +18,6 @@ param[1]="PermitRootLogin "
 param[2]="PubkeyAuthentication"
 param[3]="PasswordAuthentication"
 
-# Functions
-usage(){
-  cat << EOF
-    usage: $0 ARG1
-    ARG1 Name of the sshd_config file to edit.
-    In case ARG1 is empty, /etc/ssh/sshd_config will be used as default.
-
-    Description:
-    This script sets certain parameters in /etc/ssh/sshd_config.
-    It's not production ready and only used for training purposes.
-
-    What should it do?
-    * Check whether a /etc/ssh/sshd_config file exists
-    * Create a backup of this file
-    * Edit the file to set certain parameters
-EOF
-}
-
-backup_sshd_config(){
-  if [ -f ${file} ]
-  then
-    /usr/bin/cp ${file} ${file}.1
-  else
-    /usr/bin/echo "File ${file} not found."
-    exit 1
-  fi
-}
-
 edit_sshd_config(){
   for PARAM in ${param[@]}
   do
@@ -83,6 +55,5 @@ then
 
 file="/etc/ssh/sshd_config"
 fi
-backup_sshd_config
 edit_sshd_config
 reload_sshd
