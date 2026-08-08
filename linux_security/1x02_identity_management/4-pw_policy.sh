e5b401e170296e67fcfee1a2fab18317154aef28
JulienGirardey <juliengirardey2@gmail.com>
2026-08-08 03:15:09 -0700

    fix: quote package name in apt-get install for consistency in 4-pw_policy.sh

--- linux_security/1x02_identity_management/4-pw_policy.sh
+++ linux_security/1x02_identity_management/4-pw_policy.sh
@@ -3,7 +3,7 @@ if dpkg -s "$1" >/dev/null 2>&1; then
     echo "$1 is installed."
 else
     apt-get update
-    apt-get install -y "$1"
+    apt-get install -y $1
 fi
 sed -i 's/^#\s*minlen\s*=.*/# minlen = 12/; s/^#\s*minclass\s*=.*/# minclass = 3/' /etc/security/pwquality.conf
 pam-auth-update --package
