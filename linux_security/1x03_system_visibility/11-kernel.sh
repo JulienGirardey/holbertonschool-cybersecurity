e17a07219a150e7f9dad1ddb8a3d42a88ced0dcd
JulienGirardey <juliengirardey2@gmail.com>
2026-08-10 12:00:55 -0700

    refactor: simplify segfault checks in 11-kernel.sh

--- linux_security/1x03_system_visibility/11-kernel.sh
+++ linux_security/1x03_system_visibility/11-kernel.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-if [ -f "$1/kern.log" ]; then
-    grep 'segfault' "$1/kern.log"
-fi
-
-if [ -f "$1/messages" ]; then
-    grep 'segfault' "$1/messages"
-fi
+[ -f "$1/kern.log" ] && grep 'segfault' "$1/kern.log"
+[ -f "$1/messages" ] && grep 'segfault' "$1/messages"
