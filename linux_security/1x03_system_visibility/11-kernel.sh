a776bd788dc1bef596b77468b3186f1564e51c25
JulienGirardey <juliengirardey2@gmail.com>
2026-08-10 12:02:26 -0700

    fix: correct syntax for segfault checks in 11-kernel.sh

--- linux_security/1x03_system_visibility/11-kernel.sh
+++ linux_security/1x03_system_visibility/11-kernel.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-[ -f "$1/kern.log" ] && grep 'segfault' "$1/kern.log"
-[ -f "$1/messages" ] && grep 'segfault' "$1/messages"
+[ -f "$1/kern.log" ] && grep 'segfault' "$1/kern.log"; [ -f "$1/messages" ] && grep 'segfault' "$1/messages"
