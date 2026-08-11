e68f7922cb09638df69fc21f5dfdd7bb10a39934
JulienGirardey <juliengirardey2@gmail.com>
2026-08-11 01:09:21 -0700

    fix: add checks for SERVICES and FILE_TO_WATCH in sentinel.sh

--- linux_security/1x04_security_automation/sentinel.sh
+++ linux_security/1x04_security_automation/sentinel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[ -f "sentinel.conf" ] || exit 1; source "sentinel.conf"
+[ -f "sentinel.conf" ] || exit 1; source "sentinel.conf"; [ -n "${SERVICES+x}" ] && [ -n "${FILE_TO_WATCH+x}" ] || exit 1
