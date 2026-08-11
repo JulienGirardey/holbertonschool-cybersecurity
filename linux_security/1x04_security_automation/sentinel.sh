45a6d81ea1d02a7d0d86a5db13d4309c6b14b2bf
JulienGirardey <juliengirardey2@gmail.com>
2026-08-11 02:26:36 -0700

    feat: add check_services function to monitor and restart services in sentinel.sh

--- linux_security/1x04_security_automation/sentinel.sh
+++ linux_security/1x04_security_automation/sentinel.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 [ -f "sentinel.conf" ] || exit 1; source "sentinel.conf"; [ -n "${SERVICES+x}" ] && [ -n "${FILES_TO_WATCH+x}" ] || exit 1
+check_services() {
+    for index in "${SERVICES[@]}"; do
+        if [ "pgrep -f $index" ]; then
+            echo "OK: $index is running"
+        else
+            eval "$index"
+            if [ "pgrep -f $index" ]; then
+                echo "FIXED: Restarted $index"
+            else
+                echo "Error: " <2
+            fi
+        fi 
+    done
+}
+
+check_services
