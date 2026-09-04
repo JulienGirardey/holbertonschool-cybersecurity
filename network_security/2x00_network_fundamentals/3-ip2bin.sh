91f8ccd74f477975cf95da573753c6c97d6abd7a
JulienGirardey <juliengirardey2@gmail.com>
2026-09-04 06:11:21 -0700

    fix: remove unnecessary braces in IP to binary conversion script

--- network_security/2x00_network_fundamentals/3-ip2bin.sh
+++ network_security/2x00_network_fundamentals/3-ip2bin.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for byte in `echo ${1} | tr "." " "`; do printf "%08d." "$(echo "obase=2;$byte" | bc)"; done | sed 's/\.$//'
+for byte in `echo $1 | tr "." " "`; do printf "%08d." "$(echo "obase=2;$byte" | bc)"; done | sed 's/\.$//'
