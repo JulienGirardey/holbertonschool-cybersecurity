abfba1ae28cdbefe6ea2cc7d8d9df7caa801b651
JulienGirardey <juliengirardey2@gmail.com>
2026-09-16 04:14:34 -0700

    fix: change print to printf for consistent output in localhost IP retrieval script

--- network_security/2x01_network_services/2-hosts.sh
+++ network_security/2x01_network_services/2-hosts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk '!/^#/ && $1 ~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/ { for(i=2;i<=NF;i++) if ($i=="localhost") { print $1; exit } }' /etc/hosts
+awk '!/^#/ && $1 ~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/ { for(i=2;i<=NF;i++) if ($i=="localhost") { printf "%s", $1; exit } }' /etc/hosts
